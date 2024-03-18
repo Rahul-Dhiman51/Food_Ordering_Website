0e8c164206d13571a8e8ffdaaed5f814590664c0
Rahul-Dhiman51 <76252998+Rahul-Dhiman51@users.noreply.github.com>
2024-03-18 13:28:18 +0530

    model Model issue

--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import jwt from "jsonwebtoken";
 import { BAD_REQUEST } from "../constants/httpStatus.js";
 import handler from "express-async-handler";
-import { UserModel } from "../Models/user.model.js";
+import { UserModel } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import auth from "../middleware/auth.mid.js";
 
